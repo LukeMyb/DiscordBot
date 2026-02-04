9fcb421e575159e14444bfce1e9cc89952815ef5
LukeMyb <ryutten@icloud.com>
2026-02-04 12:58:57 +0900

    kick, banコメントアウトきれいにしただけ

--- cogs/admin.py
+++ cogs/admin.py
@@ -5,19 +5,17 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # キック機能
+    #キック
     @commands.command()
-    @commands.has_permissions(kick_members=True) # 実行者に「メンバーをキック」権限があるか確認
+    @commands.has_permissions(kick_members=True) #実行者の権限確認
     async def kick(self, ctx, member: discord.Member, *, reason=None):
-        """指定したユーザーをサーバーからキックします。"""
         await member.kick(reason=reason)
         await ctx.send(f"{member.mention} をキックしました。理由: {reason}")
 
-    # BAN機能
+    #BAN
     @commands.command()
-    @commands.has_permissions(ban_members=True) # 実行者に「メンバーをBAN」権限があるか確認
+    @commands.has_permissions(ban_members=True) #実行者の権限確認
     async def ban(self, ctx, member: discord.Member, *, reason=None):
-        """指定したユーザーをサーバーからBANします。"""
         await member.ban(reason=reason)
         await ctx.send(f"{member.mention} をBANしました。理由: {reason}")
 
