fd4eeb067e4015506a7d4ad92e636bd9ae412939
LukeMyb <ryutten@icloud.com>
2026-02-07 00:02:44 +0900

    機械学習のai.pyを追加

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class MyBot(commands.Bot): #commands.botは継承元である親クラス
         await self.load_extension("cogs.ping")
         await self.load_extension("cogs.admin")
         await self.load_extension("cogs.leveling")
+        await self.load_extension("cogs.ai")
         print(f"Logged in as {self.user}")
 
 load_dotenv() #.envの内容を環境変数としてロード
