504574e65a430fd8192b333f449b1654258d8503
LukeMyb <ryutten@icloud.com>
2026-02-19 22:47:35 +0900

    画像の出力テスト

--- cogs/meme.py
+++ cogs/meme.py
@@ -1,9 +1,39 @@
 import discord
 from discord.ext import commands
+from PIL import Image
+import io
+import os
 
 class Meme(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+        self.dragon_path = "assets/sukina-souzai-happyo-dragon.png"
+        self.robo_path = "assets/maji-res-robo.png"
+
+    @commands.command()
+    async def dragon(self, ctx):
+        if not os.path.exists(self.dragon_path): #ファイルの有無を確認
+            await ctx.send(f"エラー: {self.dragon_path} が見つかりません。")
+            return
+        
+        async with ctx.typing():
+            try:
+                with Image.open(self.dragon_path) as img: #Pillowで画像を開く
+                    buffer = io.BytesIO() #メモリ上のバッファに保存
+                    img.save(buffer, format="PNG")
+                    buffer.seek(0) #バッファの先頭に戻る
+
+                    file = discord.File(fp=buffer, filename="output.png") #Discordにファイルを送信
+                    await ctx.send("", file=file)
+            except Exception as e:
+                await ctx.send(f"画像処理中にエラーが発生しました: {e}")
+    
+    @commands.command()
+    async def robo(self, ctx):
+        if not os.path.exists(self.dragon_path): #ファイルの有無を確認
+            await ctx.send(f"エラー: {self.robo_path} が見つかりません。")
+            return
 
 async def setup(bot):
     await bot.add_cog(Meme(bot))
