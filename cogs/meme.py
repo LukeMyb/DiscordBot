dde7916ad35b4e3476dd6fed6b86ac5e1931827d
LukeMyb <ryutten@icloud.com>
2026-02-20 05:14:10 +0900

    吹き出しに納まるように文章の自動改行と自動縮小

--- cogs/meme.py
+++ cogs/meme.py
@@ -17,7 +17,7 @@ class Size: #描画を許可する範囲(幅と高さ)
     h: int
 
 @dataclass
-class DrawProps:
+class DrawProps: #吹き出しのプロパティ
     pos: Pos
     size: Size
     base_font_size: int
@@ -30,7 +30,7 @@ MEME_CONFIG = {
     ),
     "robo": DrawProps(
         pos=Pos(x=100, y=110), 
-        size=Size(w=430, h=230), 
+        size=Size(w=430-100, h=230-110), 
         base_font_size=25, 
     ),
 }
@@ -52,20 +52,45 @@ class Meme(commands.Cog):
             return ImageFont.load_default()
         
     def draw_text(self, draw, text, dp: DrawProps):
+        font_size = dp.base_font_size #文字のサイズ
+        line_spacing = 5 #行間
+
+        #枠に収まるフォントサイズと行分割を探すループ
+        while font_size > 10:
+            font = self.get_font(font_size) #フォントの適用
+            lines = [] #改行を含めた文章全体(要素間が改行を表す)
+            current_line = "" #現在扱っている文章(改行なし)
+            
+            #指定の幅(dp.size.w)で自動改行処理
+            for char in text:
+                test_line = current_line + char
+                if font.getlength(test_line) <= dp.size.w: #枠内に収まるなら改行なしでcharを文章に追加
+                    current_line = test_line
+                else:
+                    if current_line:
+                        lines.append(current_line)
+                    current_line = char #新たなラインを開始
+            lines.append(current_line)
+            
+            #全体の高さを計算
+            line_h = font.getbbox("あ")[3] - font.getbbox("あ")[1] #bbox[3] - bbox[1] (上端 - 下端)
+            total_h = len(lines) * line_h + (len(lines) - 1) * line_spacing #行数*文字の高さ + 行間の数*行間の高さ
+            
+            #枠の高さ(dp.size.h)に収まればループ終了
+            if total_h <= dp.size.h:
+                break
+            font_size -= 2 #はみ出すならサイズを下げてやり直し
+
         #枠の中心
         center_x = dp.pos.x + dp.size.w // 2
         center_y = dp.pos.y + dp.size.h // 2
 
-        text_size = dp.base_font_size #文字のサイズ
-
-        while text_size > 10: #枠内に収まるまでフォントサイズを2pxずつ下げる
-            font = self.get_font(text_size)
-            if font.getlength(text) <= dp.size.w: #実際に描画したときの幅 <= 枠の幅
-                break
-            text_size -= 2
-
-        #anchor="mm"は中央揃え
-        draw.text((center_x, center_y), text, font=font, fill=(0, 0, 0), anchor="mm")
+        #文章全体が上下中央に来るように開始位置を計算
+        start_y = center_y - (total_h // 2) + (line_h // 2) #枠の中心座標 - 枠の高さの半分 + 文字の高さの半分
+        
+        for i, line in enumerate(lines): #ラインごとに出力
+            y = start_y + i * (line_h + line_spacing) #行が変わったらその分yを下にずらす
+            draw.text((center_x, y), line, font=font, fill=(0, 0, 0), anchor="mm")
 
     @commands.command()
     async def dragon(self, ctx, *, text: str):
