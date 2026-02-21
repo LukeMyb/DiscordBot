9e61eb70ca1802f5011173a7654c7320bcbbfd91
LukeMyb <ryutten@icloud.com>
2026-02-21 13:48:55 +0900

    ログをembedに

--- cogs/log.py
+++ cogs/log.py
@@ -15,17 +15,31 @@ class Log(commands.Cog):
         log_channel = discord.utils.get(member.guild.text_channels, name="log") #「log」というチャンネル名のチャンネルを検索
         if not log_channel: return
 
+        embed = discord.Embed() #embedの初期化
+
         #入室・退室・移動の判定
         if before.channel is None and after.channel is not None: #新しくVCに参加した場合
-            msg = f"ID: {member.id}, JOIN | **{member.display_name}** が '{after.channel.name}' に参加しました。"
+            embed.color = discord.Color.green() #入室は緑色
+            embed.title = "JOIN"
+            embed.description = f"**{member.display_name}** が '{after.channel.name}' に参加しました。"
+            embed.set_footer(text=f"ID: {member.id} | ACTION: JOIN")
+
         elif before.channel is not None and after.channel is None: #VCから完全に退出した場合
-            msg = f"ID: {member.id}, LEAVE | **{member.display_name}** が '{before.channel.name}' から退出しました。"
+            embed.color = discord.Color.red() #退室は赤色
+            embed.title = "LEAVE"
+            embed.description = f"**{member.display_name}** が '{before.channel.name}' から退出しました。"
+            embed.set_footer(text=f"ID: {member.id} | ACTION: LEAVE")
+
         elif before.channel is not None and after.channel is not None: #VC間を移動した場合
-            msg = f"ID: {member.id}, MOVE | **{member.display_name}** が '{before.channel.name}' → '{after.channel.name}' へ移動しました。"
+            embed.color = discord.Color.blue() #移動は青色
+            embed.title = "MOVE"
+            embed.description = f"**{member.display_name}** が '{before.channel.name}' → '{after.channel.name}' へ移動しました。"
+            embed.set_footer(text=f"ID: {member.id} | ACTION: MOVE")
+
         else:
             return
 
-        await log_channel.send(msg)
+        await log_channel.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(Log(bot))
