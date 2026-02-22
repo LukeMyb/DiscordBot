4c74ca066d10a3f6c5f31eaf4cfc848c56cb71a6
LukeMyb <ryutten@icloud.com>
2026-02-22 15:02:07 +0900

    細かい修正

--- cogs/anonymous.py
+++ cogs/anonymous.py
@@ -1,9 +1,9 @@
 import discord
 from discord.ext import commands
 
-class Admin(commands.Cog):
+class Anonymous(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
 async def setup(bot):
-    await bot.add_cog(Admin(bot))
+    await bot.add_cog(Anonymous(bot))
